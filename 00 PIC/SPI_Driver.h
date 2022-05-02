c133578bdff4f310d001604e999fa062451ee959
C0r0naLight <100315703+C0r0naLight@users.noreply.github.com>
2022-05-01 23:33:16 -0700

    Update SPI_Driver.h
    Removed unused code
    
    added comment

--- 00 PIC/SPI_Driver.h
+++ 00 PIC/SPI_Driver.h
@@ -1,9 +1,6 @@
 #include "mcc_generated_files/mcc.h"
 
-#define delay NOP(); NOP();
-
 #define CLK LATCbits.LATC1
-//#define INPUT LATBbits.LATB5
 #define OUT LATCbits.LATC7
 #define CS LATBbits.LATB7
 
@@ -12,13 +9,6 @@ long int k = 0;
 
 int adcData[10] = {0,0,0,0,0,0,0,0,0,0};
 
-void timer();
-
-void startBits();
-
-//void sendBits();
-
-void readData();
-
+//Reads data from the ADC and sets colour of the lights
 void readADC();
 
